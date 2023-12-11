e007f5a3797b324222b352da23c9a906b25a76bd
d3w3r <d3w3r@proton.me>
2023-12-10 23:29:44 -0500

    Finished step 2 of unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,12 +20,22 @@ const Board = ({items}) => {
   const item1 = items[0];
   const item2 = items[1];
   const item3 = items[2];
+  let positive = "0 %";
+
+  const total = item1.value + item2.value + item3.value;
+  const average = Number(total / 3).toFixed(5);
+
+  if (total !== 0)
+    positive = Number((item1.value / total) * 100).toFixed(2) + " %";
 
   return (
     <div>
       <Tag name={item1.name} value={item1.value}/>
       <Tag name={item2.name} value={item2.value}/>
       <Tag name={item3.name} value={item3.value}/>
+      <Tag name="all" value={total}/>
+      <Tag name="average" value={average}/>
+      <Tag name="positive" value={positive} />
     </div>
   );
 }
