18bc674c422fc82b2560b173acbdb3e787cb49e7
d3w3r <d3w3r@proton.me>
2023-12-14 19:36:10 -0500

    Finished step 8 of courseinfo

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,15 +1,11 @@
 const Total = (props) => {
   const parts = props.course.parts;
 
-  const value =
-    parts[0].exercises +
-    parts[1].exercises +
-    parts[2].exercises +
-    parts[3].exercises;
+  const total = parts.reduce((a, b) => a + b.exercises, 0);
 
   return (
     <>
-      <p><strong>total of {value} exercises</strong></p>
+      <p><strong>total of {total} exercises</strong></p>
     </>
   );
 };
