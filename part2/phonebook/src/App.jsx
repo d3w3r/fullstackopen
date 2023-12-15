1c7fa1061282673ae5386ea6d084c1cbacc67ba9
d3w3r <d3w3r@proton.me>
2023-12-14 23:06:21 -0500

    Finished step 4 of phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,9 +6,11 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [newFilter, setNewFilter] = useState('');
+  const [filter, setFilter] = useState('false');
 
-  const trackPhone = (event) => setNewPhone(event.target.value);
   const trackName = (event) => setNewName(event.target.value);
+  const trackPhone = (event) => setNewPhone(event.target.value);
   const addName = (event) => {
     event.preventDefault();
 
@@ -39,11 +41,22 @@ const App = () => {
     setNewName('');
     setNewPhone('');
   };
+  const trackFilter = (event) => {
+    const value = event.target.value;
 
+    if (value.trim().length === 0) setFilter(false);
+    else setFilter(true);
+
+    setNewFilter(value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={newFilter} onChange={trackFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={trackName}/>
@@ -57,7 +70,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(p => <p key={p.id}>{p.name} {p.phone}</p>)}
+        {
+          filter
+            ? persons.filter(p => p.name.includes(newFilter)).map(p => <p key={p.id}>{p.name} {p.phone}</p>)
+            : persons.map(p => <p key={p.id}>{p.name} {p.phone}</p>)
+        }
       </div>
     </div>
   )
