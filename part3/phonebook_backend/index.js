3f4f51161b54132138a4f7ace6da517704d13b48
d3w3r <d3w3r@proton.me>
2024-01-27 22:15:08 -0500

    Finished step 8 of Phonebook backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan  = require('morgan')
 
-const app = express()
-
 const PORT = process.env.PORT || 3001
+
+const app = express()
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
 let PERSONS = [
   {
     "id": 1,
@@ -28,7 +32,16 @@ let PERSONS = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res),
+  ].join(' ')
+}))
 
 app.get('/api/persons', (request, response) => {
   response.json(PERSONS)
